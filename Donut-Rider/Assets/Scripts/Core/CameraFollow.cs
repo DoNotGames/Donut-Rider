05cffb2f1702213c1319467f527dc51e6132345e
Dominik Malolepszy <malolepszydominik@gmail.com>
2023-03-22 11:27:16 +0100

    CameraFollow control upgrade

--- Donut-Rider/Assets/Scripts/Core/CameraFollow.cs
+++ Donut-Rider/Assets/Scripts/Core/CameraFollow.cs
@@ -2,28 +2,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private float cameraDelay;
+    [SerializeField][Range(0,20)] private float cameraDelay;
+    [SerializeField] private Vector3 cameraOffset;
 
     private Transform playerTransform;
     private Transform mainCameraTransform;
-    private Vector3 cameraOffset;
 
     private void Awake()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         mainCameraTransform = Camera.main.transform;
-        cameraOffset = mainCameraTransform.position - playerTransform.position;
     }
 
     private void LateUpdate()
     {
-        Follow();
-    }
-
-    private void Follow()
-    {
         Vector3 desiredPositon = playerTransform.position + cameraOffset;
-        desiredPositon = new Vector3(desiredPositon.x, desiredPositon.y, mainCameraTransform.position.z);
         mainCameraTransform.position = Vector3.Slerp(mainCameraTransform.position, desiredPositon, Time.deltaTime * cameraDelay);
     }
 }
